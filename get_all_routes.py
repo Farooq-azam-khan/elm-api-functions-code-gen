8192cb1e76c62c2b3b882dd871dfb2c7bdd6c3d6
farooq-azam-khan-bdo <fkhan@bdo.ca>
2024-08-15 15:34:38 -0400

    refactored

--- get_all_routes.py
+++ get_all_routes.py
@@ -3,12 +3,19 @@ from typing import Any
 import requests
 from termcolor import colored
 
-open_api_json_req_url = "http://localhost:8000/openapi.json"
-resp = requests.get(open_api_json_req_url)
-apis = resp.json()
+local_openapi_json = "http://localhost:8000/openapi.json"
+
+
+def get_openapi_config(
+    open_api_json_req_url: str = local_openapi_json,
+) -> dict[Any, Any]:
+    resp = requests.get(open_api_json_req_url)
+    apis = resp.json()
+    return apis
 
 
 skip_non_api_routes = True
+# use_fast_api_web_data = True # TODO toggle fastapi webdata
 tab = "    "
 
 
@@ -23,7 +30,6 @@ def add_encoder_to_fn(method_vals, elm_fn_definition_dict):
         elm_request_encoder = (
             f"{tab}{tab}|> HttpBuilder.withJsonBody request_body_encoder"
         )
-        # TODO: create encoder based on apis['components']['schemas']
     return args, args_names, elm_request_encoder
 
 
@@ -40,8 +46,6 @@ def format_api_fn(
     elm_fn_body_http_fns = "\n".join(
         elm_fn_definition_dict["fn_body"]["http_builder_fns"]
     )
-
-    print(colored(f"{elm_fn_body_http_fns=}", "red"))
     formatted_fn_output = f"""
 {elm_fn_definition}
 {elm_fn_arguments}
@@ -80,6 +84,26 @@ def add_url_parameters_to_fn(
     return elm_route, args, args_names
 
 
+def create_response_type(method_vals):
+    if "responses" in method_vals:
+        responses = method_vals["responses"]
+        print(f"responses ({len(responses.keys())})=")
+        for resp_key, resp_val in method_vals["responses"].items():
+            print(colored(f"\t{resp_key}={resp_val}", "yellow"))
+            if "content" in resp_val:
+                response_content_schema = resp_val["content"]["application/json"][
+                    "schema"
+                ]
+                schemas_count = len(response_content_schema.keys())
+                if schemas_count == 0:
+                    print(
+                        colored(
+                            "\tWARN: schema response not defined",
+                            "yellow",
+                        )
+                    )
+
+
 def create_api_functions(apis: dict[Any, Any]):
     print(colored("Assuming everyting is content-type: application/json", "red"))
     print(
@@ -91,8 +115,8 @@ def create_api_functions(apis: dict[Any, Any]):
     print(colored("Assume url param name is a valid elm variable", "red"))
     # TODO: create a dict that stores function name.
     # TODO: if there is a duplicate then make sure to tell user and add random 4 letter digit to it at the end.
-    elm_functions = []
-    # elm_functions = {}
+    # elm_functions = []
+    elm_functions = {}
     # [{fn_name: str, args_names:[str], args:[str], fn_body: [str], output_arg: str}]
     # fn_body is list of http builder functions
     for route, methods in apis["paths"].items():
@@ -103,11 +127,12 @@ def create_api_functions(apis: dict[Any, Any]):
         print(colored(f"{route=}", "yellow"))
 
         for method, method_vals in methods.items():
+            operation_id = method_vals["operationId"]
+            print(colored(f"{operation_id=} ", "yellow"))
             print("keys=", method_vals.keys())
-            elm_function_name = method_vals["operationId"]
             elm_fn_definition_dict = {
-                "fn_name": elm_function_name,
-                "args": ["(WebData a -> msg)", "D.Decoder a"],
+                "fn_name": operation_id,
+                "args": ["(FastApiWebData a -> msg)", "D.Decoder a"],
                 "args_names": ["msg", "decoder"],
                 "output_arg": "Cmd msg",
                 "fn_body": {
@@ -115,7 +140,7 @@ def create_api_functions(apis: dict[Any, Any]):
                     "http_method": method,
                     "http_builder_fns": [
                         f"{tab}{tab}|> HttpBuilder.withTimeout 90000",
-                        f"{tab}{tab}|> HttpBuilder.withExpect\n{tab}{tab}{tab}(Http.expectJson (RemoteData.fromResult >> msg) decoder)",
+                        f"{tab}{tab}|> HttpBuilder.withExpect\n{tab}{tab}{tab}(expect_fast_api_response (RemoteData.fromResult >> msg) decoder)",
                         f"{tab}{tab}|> HttpBuilder.request",
                     ],
                 },
@@ -140,33 +165,20 @@ def create_api_functions(apis: dict[Any, Any]):
             )
             elm_fn_definition_dict["fn_body"]["route"] = elm_route
 
-            print(colored(elm_fn_definition_dict, "red"))
+            # print(colored(elm_fn_definition_dict, "red"))
             formatted_fn_output = format_api_fn(
                 elm_fn_definition_dict,
                 method,
                 elm_request_encoder,
             )
-
-            if "responses" in method_vals:
-                happy_path = method_vals["responses"]["200"]
-                print("\tresponses=")
-                happy_path_response_schema = happy_path["content"]["application/json"][
-                    "schema"
-                ]
-                print(
-                    "\t",
-                    f"200 schemas={len(happy_path_response_schema.keys())}",
-                    happy_path,
-                )
-                if len(happy_path_response_schema.keys()) == 0:
-                    print(colored("HELP: define response schema", "yellow"))
-
-                # TODO: consider non happy path
-                # for resp_key, resp_val in method_vals['responses'].items():
-                #     print('\t', resp_key, resp_val)
+            create_response_type(method_vals)
 
             print(colored(formatted_fn_output, "green"))
-            elm_functions.append(formatted_fn_output)
+            if elm_fn_definition_dict["fn_name"] in elm_functions:
+                print(
+                    colored(f'ERR: {elm_fn_definition_dict["fn_name"]} already exists')
+                )
+            elm_functions[elm_fn_definition_dict["fn_name"]] = formatted_fn_output
     return elm_functions
 
 
@@ -230,20 +242,110 @@ class DBQuery(BaseModel):
     query: str
 """
 
+elm_expect_fastpai_fn_and_types = """
+type FastApiHttpError
+    = BadUrl String
+    | Timeout
+    | NetworkError
+    | BadStatus Int ValidationError
+    | BadBody String
 
-def write_http_fns_file(output_file):
-    print(colored("writing file", "green"))
-    elm_functions = create_api_functions(apis)
-    print(colored(f"writing {len(elm_functions)} api functions", "green"))
-    functions_str = "\n\n".join(elm_functions)
-    file_content = f"""
-module ApiGen exposing(..)
+type alias FastApiWebData a =
+    RemoteData FastApiHttpError a
 
+
+
+type LocType
+    = StringLoc String
+    | IntLoc Int
+
+
+type alias ValidationError =
+    { detail : List ValidationErrorDetail }
+
+
+type alias ValidationErrorDetail =
+    { loc : List LocType, msg : String, type_ : String }
+
+
+loc_decoder : D.Decoder LocType
+loc_decoder =
+    D.oneOf [ D.string |> D.map StringLoc, D.int |> D.map IntLoc ]
+
+
+decode_validation_error : D.Decoder ValidationError
+decode_validation_error =
+    D.map ValidationError
+        (D.field "detail" (D.list decode_ValidationErrorDetail))
+
+
+decode_ValidationErrorDetail : D.Decoder ValidationErrorDetail
+decode_ValidationErrorDetail =
+    D.map3 ValidationErrorDetail
+        (D.field "loc" (D.list loc_decoder))
+        (D.field "msg" D.string)
+        (D.field "type" D.string)
+
+
+expect_fast_api_response : (Result FastApiHttpError value -> msg) -> D.Decoder value -> Http.Expect msg
+expect_fast_api_response to_msg decoder =
+    Http.expectStringResponse
+        to_msg
+        (\response ->
+            case response of
+                Http.BadUrl_ url ->
+                    Err <| BadUrl url
+
+                Http.Timeout_ ->
+                    Err <| Timeout
+
+                Http.NetworkError_ ->
+                    Err <| NetworkError
+
+                Http.BadStatus_ metadata str_body ->
+                    Err <|
+                        BadStatus metadata.statusCode <|
+                            case D.decodeString decode_validation_error str_body of
+                                Ok value ->
+                                    value
+
+                                Err err ->
+                                    { detail = [ { loc = [], msg = D.errorToString err, type_ = "" } ] }
+
+                Http.GoodStatus_ _ str_body ->
+                    case D.decodeString decoder str_body of
+                        Ok value ->
+                            Ok value
+
+                        Err err ->
+                            Err (BadBody (D.errorToString err))
+        )
+""".strip()
+
+elm_imports = """
 import Http
 import HttpBuilder
 import Json.Decode as D
 import Json.Encode as E
-import RemoteData exposing (RemoteData(..), WebData)
+import RemoteData exposing (RemoteData(..))
+""".strip()
+
+
+def write_http_fns_file(
+    elm_functions: list[Any], output_file: str, open_api_version: str
+) -> None:
+    print(colored("writing file", "green"))
+    print(colored(f"writing {len(elm_functions)} api functions", "green"))
+    functions_str = "\n\n".join(
+        [elm_fn_formatted for _, elm_fn_formatted in elm_functions.items()]
+    )
+    file_content = f"""
+module ApiGen exposing(..)
+-- GENRATED FOR OPENAPI={open_api_version}
+
+{elm_imports}
+
+{elm_expect_fastpai_fn_and_types}
 
 {functions_str}
     """.strip()
@@ -254,7 +356,13 @@ import RemoteData exposing (RemoteData(..), WebData)
 
 if __name__ == "__main__":
     try:
-        write_http_fns_file(output_file="./codegen/ApiGen.elm")
-        # write_encoders(apis)
+        open_api_json_req_url = local_openapi_json
+        apis = get_openapi_config(open_api_json_req_url)
+        elm_functions = create_api_functions(apis)
+        write_http_fns_file(
+            elm_functions,
+            output_file="./codegen/ApiGen.elm",
+            open_api_version=apis["openapi"],
+        )
     except requests.exceptions.ConnectionError:
         print(f"is {open_api_json_req_url} running?")
